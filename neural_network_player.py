98e7d953e96fbb8fba46bcc556a29652d967dbf1
lefos99 <lefosm@gmail.com>
2019-04-26 13:42:06 +0300

    Added a plot to show the accuracy through epochs and got rid of some features of the model.

--- neural_network_player.py
+++ neural_network_player.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import LabelEncoder
 from keras.models import model_from_json
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # fix random seed for reproducibility
 seed = 7
@@ -13,12 +14,15 @@ np.random.seed(seed)
 
 # load data
 gameplay_data = pd.read_csv("./snake_data.csv")
-X = gameplay_data.drop(['NewDir'], axis=1)
-print(X)
-X = np.array(X.values)
-# ~ X = X[:,0:10]
 Y = gameplay_data['NewDir']
-print(X)
+X = gameplay_data.drop(['NewDir',
+						'OldDir',
+						'snakeStartX','snakeStartY',
+						# ~ 'snakeMidX','snakeMidY',
+						'snakeEndX','snakeEndY','SnakeLength'], axis=1)
+X = np.array(X.values)
+n_features = X.shape[1]
+print("Using number of features: " + str(X.shape[1]))
 
 # encode class values as integers
 encoder = LabelEncoder()
@@ -29,22 +33,30 @@ Y = np_utils.to_categorical(encoded_Y)
 
 # create model
 model = Sequential()
-model.add(Dense(30, input_shape=(12,), activation='relu'))
+model.add(Dense(20, input_shape=(n_features,), activation='relu'))
 model.add(Dense(15, activation='relu'))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(5, activation='relu'))
-model.add(Dense(5, activation='relu'))
+model.add(Dense(8, activation='relu'))
 model.add(Dense(4, activation='softmax')) 
 
 # compile model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # fit the model
-model.fit(X, Y, epochs=300, batch_size=10)
+history = model.fit(X, Y, epochs=150, batch_size=10)
 
 # evaluate the model
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('model/model_accuracy.png')
 
 # serialize model to JSON
 model_json = model.to_json()
